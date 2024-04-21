398fec11dcabf5c012060118d404ebe4bce020f0
Shreya Sakshi <shreyasakshi100@gmail.com>
2024-04-21 12:54:44 +0530

    Diamond pattern and reverse of number

---
+++ reverseOfNum.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class reverseOfNum {
+    
+    public static int reverse(int n) {
+		
+		int rev = 0;
+		while(n>0) {
+			int lastDigit = n%10;
+			rev = rev*10+lastDigit;
+			n = n/10;
+		}
+		return rev;
+	}
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int r = reverse(n);
+		System.out.println(r);
+	}
+}
